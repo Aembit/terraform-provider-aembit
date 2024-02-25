55f7a255bb9e5099da4a0e1d0cc175a796be73ba
David Walker <david.walker@quantifex.com>
2024-02-24 22:41:02 -0500

    BAC-2595: Fix for duplication issues during parallel acceptance testing

--- internal/provider/client_workload_resource_test.go
+++ internal/provider/client_workload_resource_test.go
@@ -1,7 +1,10 @@
 package provider
 
 import (
+	"fmt"
+	"math/rand"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -11,12 +14,16 @@ func TestAccClientWorkloadResource(t *testing.T) {
 	createFile, _ := os.ReadFile("../../tests/client/TestAccClientWorkloadResource.tf")
 	modifyFile, _ := os.ReadFile("../../tests/client/TestAccClientWorkloadResource.tfmod")
 
+	randID := rand.Intn(10000000)
+	createFileConfig := strings.ReplaceAll(string(createFile), "unittest1namespace", fmt.Sprintf("unittest1namespace%d", randID))
+	modifyFileConfig := strings.ReplaceAll(string(modifyFile), "unittest1namespace", fmt.Sprintf("unittest1namespace%d", randID))
+
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Create and Read testing
 			{
-				Config: string(createFile),
+				Config: createFileConfig,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify Client Workload Name, Description, Active status
 					resource.TestCheckResourceAttr("aembit_client_workload.test", "name", "Unit Test 1"),
@@ -39,7 +46,7 @@ func TestAccClientWorkloadResource(t *testing.T) {
 			},
 			// Update and Read testing
 			{
-				Config: string(modifyFile),
+				Config: modifyFileConfig,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify Name updated
 					resource.TestCheckResourceAttr("aembit_client_workload.test", "name", "Unit Test 1 - modified"),
