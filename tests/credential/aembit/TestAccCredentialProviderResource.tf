93f78cac2cc1da2839102c9dd2d1e35bccb01323
David Walker <david.walker@quantifex.com>
2024-03-06 00:13:38 -0500

    BAC-2635: Updated with temporary RoleID

--- tests/credential/aembit/TestAccCredentialProviderResource.tf
+++ tests/credential/aembit/TestAccCredentialProviderResource.tf
@@ -5,7 +5,7 @@ resource "aembit_credential_provider" "aembit" {
 	name = "TF Acceptance Aembit Token"
 	is_active = true
 	aembit_access_token = {
-		role_id = "87e3c07e-afa2-49cf-b585-769c71557d02"
+		role_id = "cca45e0a-c1c6-4b24-a895-52b547861a28"
 		lifetime = 900
 	}
 }
