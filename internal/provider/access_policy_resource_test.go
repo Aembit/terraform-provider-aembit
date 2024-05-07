01e1e6d70f096f7351e482f5a6ba15607b036911
David Walker <david.walker@quantifex.com>
2024-05-07 16:02:03 -0400

    BAC-2850: Ensure Access Policies don't require Access Conditions

--- internal/provider/access_policy_resource_test.go
+++ internal/provider/access_policy_resource_test.go
@@ -49,3 +49,43 @@ func TestAccAccessPolicyResource(t *testing.T) {
 		},
 	})
 }
+
+func TestAccBasicAccessPolicyResource(t *testing.T) {
+	createFile, _ := os.ReadFile("../../tests/policy/TestAccBasicAccessPolicyResource.tf")
+	modifyFile, _ := os.ReadFile("../../tests/policy/TestAccBasicAccessPolicyResource.tfmod")
+
+	randID := rand.Intn(10000000)
+	createFileConfig := strings.ReplaceAll(string(createFile), "clientworkloadNamespace", fmt.Sprintf("clientworkloadNamespace%d", randID))
+	modifyFileConfig := strings.ReplaceAll(string(modifyFile), "clientworkloadNamespace", fmt.Sprintf("clientworkloadNamespace%d", randID))
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: createFileConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify dynamic values have any value set in the state.
+					resource.TestCheckResourceAttrSet("aembit_access_policy.first_policy", "id"),
+					// Verify placeholder ID is set
+					resource.TestCheckResourceAttrSet("aembit_access_policy.first_policy", "id"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      "aembit_access_policy.first_policy",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Update and Read testing
+			{
+				Config: modifyFileConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify Name updated
+					resource.TestCheckResourceAttrSet("aembit_access_policy.first_policy", "id"),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
