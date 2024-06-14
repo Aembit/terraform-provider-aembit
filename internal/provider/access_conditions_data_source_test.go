83a944b02367c5f8dce04edef82f17faa0f4c9ac
Jason Kwon <jkwon@aembit.io>
2024-06-14 14:55:24 +0900

    OPS-726: Remove unused code in Access Conditions acceptance tests

--- internal/provider/access_conditions_data_source_test.go
+++ internal/provider/access_conditions_data_source_test.go
@@ -10,9 +10,6 @@ import (
 func TestAccAccessConditionsDataSource(t *testing.T) {
 	createFile, _ := os.ReadFile("../../tests/condition/data/TestAccAccessConditionsDataSource.tf")
 
-	//randID := rand.Intn(10000000)
-	//createFileConfig := strings.ReplaceAll(string(createFile), "unittest1namespace", fmt.Sprintf("unittest1namespace%d", randID))
-
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
@@ -35,47 +32,3 @@ func TestAccAccessConditionsDataSource(t *testing.T) {
 		},
 	})
 }
-
-/*
-func testAccCheckExampleWidgetValues(widget *example.Widget, name string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		if *widget.Active != true {
-			return fmt.Errorf("bad active state, expected \"true\", got: %#v", *widget.Active)
-		}
-		if *widget.Name != name {
-			return fmt.Errorf("bad name, expected \"%s\", got: %#v", name, *widget.Name)
-		}
-		return nil
-	}
-}
-
-// testAccCheckExampleResourceExists queries the API and retrieves the matching Widget.
-func testAccCheckAccessConditionExists(accessConditionID string, accessCondition *aembit.AccessConditionDTO) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		// find the corresponding state object
-		rs, ok := s.RootModule().Resources[accessConditionID]
-		if !ok {
-			return fmt.Errorf("Access Condition ID not found: %s", accessConditionID)
-		}
-
-		// retrieve the configured client from the test setup
-		conn := testAccProvider.Meta().(*aembit.CloudClient)
-		resp, err := conn.DescribeWidget(&example.DescribeWidgetsInput{
-			WidgetIdentifier: rs.Primary.ID,
-		})
-
-		if err != nil {
-			return err
-		}
-
-		if resp.Widget == nil {
-			return fmt.Errorf("Widget (%s) not found", rs.Primary.ID)
-		}
-
-		// assign the response Widget attribute to the widget pointer
-		*widget = *resp.Widget
-
-		return nil
-	}
-}
-*/
