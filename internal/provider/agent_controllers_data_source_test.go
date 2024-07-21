7e2f5849dd59e1209d5c3d3391ac96b6f58ef212
Jason Kwon <jkwon@aembit.io>
2024-07-21 14:26:27 -0700

    OPS-846: Update Agent Controllers  Data Source acceptance test

--- internal/provider/agent_controllers_data_source_test.go
+++ internal/provider/agent_controllers_data_source_test.go
@@ -1,34 +1,50 @@
 package provider
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
 const testAgentControllersDataSource string = "data.aembit_agent_controllers.test"
+const testAgentControllerResource string = "aembit_agent_controller.azure_tp"
+
+func testFindAgentController(resourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		var rs *terraform.ResourceState
+		var err error
+		var ok bool
+		var notFound bool
+		if rs, ok = s.RootModule().Resources[resourceName]; !ok {
+			return fmt.Errorf("Not found: %s", resourceName)
+		}
+		if _, err, notFound = testClient.GetAgentController(rs.Primary.ID, nil); notFound {
+			return err
+		}
+		return nil
+	}
+}
 
 func TestAccAgentControllersDataSource(t *testing.T) {
 	createFile, _ := os.ReadFile("../../tests/agent_controllers/data/TestAccAgentControllersDataSource.tf")
+	createFileConfig, _, _ := randomizeFileConfigs(string(createFile), "", "TF Acceptance Azure Trust Provider")
 
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Read testing
 			{
-				Config: string(createFile),
+				Config: createFileConfig,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					// Verify number of Agent Controllers returned
+					// Verify non-zero number of Agent Controllers returned
 					resource.TestCheckResourceAttrSet(testAgentControllersDataSource, "agent_controllers.#"),
-					// Verify Trust Provider Name
-					resource.TestCheckResourceAttr(testAgentControllersDataSource, "agent_controllers.0.name", "TF Acceptance Azure Trust Provider"),
-					// Verify Tags.
-					resource.TestCheckResourceAttr(testAgentControllersDataSource, "agent_controllers.0.tags.%", "2"),
-					resource.TestCheckResourceAttr(testAgentControllersDataSource, "agent_controllers.0.tags.color", "blue"),
-					resource.TestCheckResourceAttr(testAgentControllersDataSource, "agent_controllers.0.tags.day", "Sunday"),
 					// Verify dynamic values have any value set in the state.
 					resource.TestCheckResourceAttrSet(testAgentControllersDataSource, "agent_controllers.0.id"),
+					// Find newly created entry
+					testFindAgentController(testAgentControllerResource),
 				),
 			},
 		},
