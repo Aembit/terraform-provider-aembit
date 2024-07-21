ce4477c7f98cde5882bf0d51b3430877740881d6
Jason Kwon <jkwon@aembit.io>
2024-07-21 14:55:40 -0700

    OPS-846: Update Trust Providers Source acceptance test

--- internal/provider/trust_providers_data_source_test.go
+++ internal/provider/trust_providers_data_source_test.go
@@ -1,34 +1,50 @@
 package provider
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
 const testTrustProvidersDataSource string = "data.aembit_trust_providers.test"
+const testTrustProviderResource string = "aembit_trust_provider.kubernetes_key"
+
+func testFindTrustProvider(resourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		var rs *terraform.ResourceState
+		var err error
+		var ok bool
+		var notFound bool
+		if rs, ok = s.RootModule().Resources[resourceName]; !ok {
+			return fmt.Errorf("Not found: %s", resourceName)
+		}
+		if _, err, notFound = testClient.GetTrustProvider(rs.Primary.ID, nil); notFound {
+			return err
+		}
+		return nil
+	}
+}
 
 func TestAccTrustProvidersDataSource(t *testing.T) {
 	createFile, _ := os.ReadFile("../../tests/trust/data/TestAccTrustProvidersDataSource.tf")
+	createFileConfig, _, _ := randomizeFileConfigs(string(createFile), "", "TF Acceptance Kubernetes")
 
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Read testing
 			{
-				Config: string(createFile),
+				Config: createFileConfig,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					// Verify number of Trust Providers returned
+					// Verify non-zero number of Trust Providers returned
 					resource.TestCheckResourceAttrSet(testTrustProvidersDataSource, "trust_providers.#"),
-					// Verify First Trust Provider Name
-					resource.TestCheckResourceAttr(testTrustProvidersDataSource, "trust_providers.0.name", "TF Acceptance Kubernetes"),
 					// Verify dynamic values have any value set in the state.
 					resource.TestCheckResourceAttrSet(testTrustProvidersDataSource, "trust_providers.0.id"),
-					// Verify Tags.
-					resource.TestCheckResourceAttr(testTrustProvidersDataSource, "trust_providers.0.tags.%", "2"),
-					resource.TestCheckResourceAttr(testTrustProvidersDataSource, "trust_providers.0.tags.color", "blue"),
-					resource.TestCheckResourceAttr(testTrustProvidersDataSource, "trust_providers.0.tags.day", "Sunday"),
+					// Find newly created entry
+					testFindTrustProvider(testTrustProviderResource),
 				),
 			},
 		},
