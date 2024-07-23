f7f853e60a5e84f786d6c8a94920f06ae468d1ae
David Walker <david.walker@quantifex.com>
2024-07-23 12:13:45 -0400

    Verbiage update

--- internal/provider/credential_provider_resource.go
+++ internal/provider/credential_provider_resource.go
@@ -333,7 +333,7 @@ func (r *credentialProviderResource) Schema(_ context.Context, _ resource.Schema
 						},
 					},
 					"lifetime_expiration": schema.StringAttribute{
-						Description: "ISO 8601 formatted Lifetime Expiration of the OAuth Authorization Code credentials requested by the Credential Provider. This is based on the last successful Authorization of the Credential Provider",
+						Description: "ISO 8601 formatted Lifetime Expiration of the OAuth Authorization Code credentials requested by the Credential Provider. This expiration timer begins when the user successfully completes an authorization of the Credential Provider and will be set to the authorization time plus the Credential Provider Lifetime value at that moment.",
 						Computed:    true,
 					},
 				},
