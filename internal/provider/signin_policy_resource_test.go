0fe04fc2dbba70db256e9db7c6de42c8bba9016d
Burak <burak@aembit.io>
2024-11-28 17:19:24 +0300

    BAC-3628: SignIn Policy settings Terraform support

---
+++ internal/provider/signin_policy_resource_test.go
@@ -0,0 +1,42 @@
+package provider
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+const testSignInPolicy string = "aembit_signin_policy.test"
+
+func TestAccSigninPolicy(t *testing.T) {
+	createFile, _ := os.ReadFile("../../tests/signin_policy/TestAccSignInPolicyResource.tf")
+	modifyFile, _ := os.ReadFile("../../tests/signin_policy/TestAccSignInPolicyResource.tfmod")
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create
+			{
+				Config: string(createFile),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(testSignInPolicy, "mfa_required", "true"),
+					resource.TestCheckResourceAttr(testSignInPolicy, "sso_required", "true"),
+				),
+				ResourceName: testSignInPolicy,
+			},
+			// ImportState testing
+			{ResourceName: testSignInPolicy, ImportState: true, ImportStateVerify: true},
+			// Update
+			{
+				Config: string(modifyFile),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(testSignInPolicy, "mfa_required", "true"),
+					resource.TestCheckResourceAttr(testSignInPolicy, "sso_required", "false"),
+				),
+				ResourceName: testSignInPolicy,
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
