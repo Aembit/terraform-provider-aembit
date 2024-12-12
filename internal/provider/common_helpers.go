4c2ebdcc63ef3d9c2443a4775872ed6c8a2d9394
Burak <burak@aembit.io>
2024-12-12 17:38:38 +0300

    BAC-3628: SignIn Policy settings Terraform support (#102)
    * BAC-3628: SignIn Policy settings Terraform support
    
    * adding tenantid to uniqueidentifier
    
    * skipCI method added
    
    * Run TestAccSigninPolicy for specific TF version
    
    * Update signin_policy_resource_test.go
    
    * Update signin_policy_resource_test.go
    
    * Update signin_policy_resource.go
    
    * Accessing tenantId from provider object
    
    * Update docs/resources/signin_policy.md
    
    * Update internal/provider/signin_policy_resource.go
    
    ---------
    
    Co-authored-by: David Walker <david.walker@quantifex.com>

--- internal/provider/common_helpers.go
+++ internal/provider/common_helpers.go
@@ -2,7 +2,10 @@ package provider
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 	"testing"
 
 	"aembit.io/aembit"
@@ -43,6 +46,21 @@ func newHTTPHeadersModel(ctx context.Context, headers []aembit.KeyValuePair) typ
 // skipNotCI can be used to skip tests which can ONLY run on GitHub.
 func skipNotCI(t *testing.T) {
 	if os.Getenv("CI") == "" {
-		t.Skip("Skipping testing in CI environment")
+		t.Skip("Skipping testing in non CI environment")
 	}
+}
+
+func getTerraformVersion() string {
+	cmd := exec.Command("terraform", "version")
+	output, err := cmd.Output()
+
+	if err != nil {
+		fmt.Printf("Error executing command: %v\n", err)
+		return "v1.6" // return the lowest version if something goes wrong
+	}
+
+	terraformVersion := strings.Split(strings.TrimSpace(string(output)), "\n")[0]
+	fmt.Println(terraformVersion)
+
+	return terraformVersion
 }
---
+++ internal/provider/signin_policy_resource_test.go
@@ -0,0 +1,84 @@
+package provider
+
+import (
+	"os"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+const testSignInPolicy string = "aembit_signin_policy.test"
+
+func TestAccSigninPolicy(t *testing.T) {
+	tfVersion := getTerraformVersion()
+	if !strings.Contains(tfVersion, "v1.9") {
+		t.Skip("Skipping testing in Terraform version " + tfVersion)
+	}
+
+	createFile, _ := os.ReadFile("../../tests/signin_policy/TestAccSignInPolicyResource.tf")
+	modifyFile, _ := os.ReadFile("../../tests/signin_policy/TestAccSignInPolicyResource.tfmod")
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create
+			{
+				Config: string(createFile),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(testSignInPolicy, "mfa_required", "true"),
+					resource.TestCheckResourceAttr(testSignInPolicy, "sso_required", "true"),
+				),
+				ResourceName: testSignInPolicy,
+			},
+			// ImportState testing
+			{ResourceName: testSignInPolicy, ImportState: true, ImportStateVerify: true},
+			// Update
+			{
+				Config: string(modifyFile),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(testSignInPolicy, "mfa_required", "true"),
+					resource.TestCheckResourceAttr(testSignInPolicy, "sso_required", "false"),
+				),
+				ResourceName: testSignInPolicy,
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func TestAccSigninPolicy_MissingRequiredAttributes(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: `
+resource "aembit_signin_policy" "test" {
+  // Missing required attributes: sso_required, mfa_required
+}
+`,
+				ExpectError: regexp.MustCompile("Missing required argument"),
+			},
+		},
+	})
+}
+
+func TestAccSigninPolicy_InvalidAttributes(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: `
+resource "aembit_signin_policy" "test" {
+    sso_required = "invalid_bool" // Should be a boolean
+  	mfa_required  = true
+}
+`,
+				ExpectError: regexp.MustCompile(`Inappropriate value for attribute`),
+			},
+		},
+	})
+}
