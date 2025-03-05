21bbb27158ae7bc11b7c8163f7da71d2e5dea5b4
auzunel-broadangle <ali.uzunel@broadangle.com>
2025-03-05 23:30:47 +0300

    Unit test update.

--- tests/client/data/TestAccClientWorkloadsDataSource.tf
+++ tests/client/data/TestAccClientWorkloadsDataSource.tf
@@ -3,7 +3,6 @@ provider "aembit" {
 
 resource "aembit_standalone_certificate_authority" "first_ca" {
     name = "first terraform standalone certificate authority"
-    is_active = true
     leaf_lifetime = 60
 }
 
