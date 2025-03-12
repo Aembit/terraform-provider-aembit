acfd0e053023e0ba400b8424ba2fde6d8f9555db
David Walker <david.walker@quantifex.com>
2025-03-12 15:21:24 -0400

    BAC-4192: Migrating Kerberos Trust Provider realm to realm_domain (#115)
    * BAC-4192: Migrating Kerberos Trust Provider realm to realm_domain
    
    * BAC-4192: Doc updates
    
    * BAC-4192: Migrate completely from realm to realm_domain
    
    * BAC-4192: Doc updates
    
    * BAC-4192: Doc updates

--- tests/trust/kerberos/TestAccTrustProviderResource.tf
+++ tests/trust/kerberos/TestAccTrustProviderResource.tf
@@ -6,7 +6,7 @@ resource "aembit_trust_provider" "kerberos" {
 	is_active = true
 	kerberos = {
 		agent_controller_ids = ["fb1b63df-bfc2-4ff9-baa4-fc84bdf9a7e5"]
-		realm = "realm"
+		realm_domain = "realm_domain"
 		principal = "principal"
 		source_ip = "source_ip"
 	}
