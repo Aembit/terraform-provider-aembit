ef6591c498a4e5982b8d04e07b32267de99726fb
David Walker <david.walker@quantifex.com>
2025-04-04 14:27:01 -0400

    BAC-4408: AgentController AllowedTLSHostname support

--- internal/provider/models/agent_controller_models.go
+++ internal/provider/models/agent_controller_models.go
@@ -7,12 +7,13 @@ import (
 // models.AgentControllerResourceModel maps the resource schema.
 type AgentControllerResourceModel struct {
 	// ID is required for Framework acceptance testing
-	ID              types.String `tfsdk:"id"`
-	Name            types.String `tfsdk:"name"`
-	Description     types.String `tfsdk:"description"`
-	IsActive        types.Bool   `tfsdk:"is_active"`
-	Tags            types.Map    `tfsdk:"tags"`
-	TrustProviderID types.String `tfsdk:"trust_provider_id"`
+	ID                 types.String `tfsdk:"id"`
+	Name               types.String `tfsdk:"name"`
+	Description        types.String `tfsdk:"description"`
+	IsActive           types.Bool   `tfsdk:"is_active"`
+	Tags               types.Map    `tfsdk:"tags"`
+	TrustProviderID    types.String `tfsdk:"trust_provider_id"`
+	AllowedTLSHostname types.String `tfsdk:"allowed_tls_hostname"`
 }
 
 // agentControllerDataSourceModel maps the datasource schema.
