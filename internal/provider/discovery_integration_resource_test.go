0732acf605c4ee16d4423dc9f2c09190a54664ba
auzunel-broadangle <54139749+auzunel-broadangle@users.noreply.github.com>
2025-04-21 17:25:57 +0300

    BAC-4215: Add Discovery integration management (#120)
    * Added support for Discovery Integrations.
    
    * Aembit API Client update.
    
    * Description update for discovery integration related resource/datasource.
    
    * Discovery integration description update for properties.
    
    * Api client update.
    
    * Discovery integration validation updates.
    
    * Update json serialization error handling for discovery integration
    
    * API Client update.

---
+++ internal/provider/discovery_integration_resource_test.go
@@ -0,0 +1,44 @@
+package provider
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+const testDiscoveryIntegrationResourceWiz string = "aembit_discovery_integration.wiz"
+
+func TestAccDiscoveryIntegrationResource_Wiz(t *testing.T) {
+	createFile, _ := os.ReadFile("../../tests/discovery_integration/wiz/TestAccDiscoveryIntegrationResource.tf")
+	modifyFile, _ := os.ReadFile("../../tests/discovery_integration/wiz/TestAccDiscoveryIntegrationResource.tfmod")
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: string(createFile),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify AccessCondition Name
+					resource.TestCheckResourceAttr(testDiscoveryIntegrationResourceWiz, "name", "TF Acceptance Wiz"),
+					// Verify dynamic values have any value set in the state.
+					resource.TestCheckResourceAttrSet(testDiscoveryIntegrationResourceWiz, "id"),
+					// Verify placeholder ID is set
+					resource.TestCheckResourceAttrSet(testDiscoveryIntegrationResourceWiz, "id"),
+				),
+			},
+			// ImportState testing
+			{ResourceName: testDiscoveryIntegrationResourceWiz, ImportState: true, ImportStateVerify: false},
+			// Update and Read testing
+			{
+				Config: string(modifyFile),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify Name updated
+					resource.TestCheckResourceAttr(testDiscoveryIntegrationResourceWiz, "name", "TF Acceptance Wiz - Modified"),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
