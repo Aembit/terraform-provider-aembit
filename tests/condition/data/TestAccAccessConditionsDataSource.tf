de40dfd873963ae03fb1bd6f827e3d089caa0019
David Walker <david.walker@quantifex.com>
2025-06-09 16:56:13 -0400

    BAC-4646: Updated Integration testing fixing URLs

--- tests/condition/data/TestAccAccessConditionsDataSource.tf
+++ tests/condition/data/TestAccAccessConditionsDataSource.tf
@@ -3,12 +3,12 @@ provider "aembit" {
 
 resource "aembit_integration" "crowdstrike" {
 	name = "TF Acceptance Crowdstrike"
-	is_active = true
+	is_active = false
 	type = "CrowdStrike"
 	sync_frequency = 3600
-	endpoint = "https://endpoint"
+	endpoint = "https://api.crowdstrike.com"
 	oauth_client_credentials = {
-		token_url = "https://url/token"
+		token_url = "https://api.crowdstrike.com/oauth2/token"
 		client_id = "client_id"
 		client_secret = "client_secret"
 		audience = "audience"
@@ -17,7 +17,7 @@ resource "aembit_integration" "crowdstrike" {
 
 resource "aembit_access_condition" "crowdstrike" {
 	name = "TF Acceptance Crowdstrike"
-	is_active = true
+	is_active = false
 	integration_id = aembit_integration.crowdstrike.id
 	crowdstrike_conditions = {
 		max_last_seen = 3600
