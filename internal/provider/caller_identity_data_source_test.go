d6ebec32e998fd90e1b8bb698578cc4d5081558d
auzunel-broadangle <ali.uzunel@broadangle.com>
2025-08-04 17:20:36 +0300

    Client Identity Datasource test updates.

--- internal/provider/caller_identity_data_source_test.go
+++ internal/provider/caller_identity_data_source_test.go
@@ -1,51 +1,51 @@
 package provider
 
 import (
-	"fmt"
-	"math/rand"
-	"os"
-	"testing"
+    "os"
+    "testing"
 
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/terraform"
+    "github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 const (
-	testCallerIdentityDataSource = "data.aembit_caller_identity.test"
+    testCallerIdentityDataSource         = "data.aembit_caller_identity.test"
+    testCallerIdentitySecondaryDataSource = "data.aembit_caller_identity.secondary"
 )
 
-// Basic check that tenant_id is present in state
-func testCallerIdentityTenantID(resourceName string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[resourceName]
-		if !ok {
-			return fmt.Errorf("not found: %s", resourceName)
-		}
-		if tenantID, ok := rs.Primary.Attributes["tenant_id"]; !ok || tenantID == "" {
-			return fmt.Errorf("expected tenant_id to be set, got: %v", tenantID)
-		}
-		return nil
-	}
+func TestAccCallerIdentityDataSource(t *testing.T) {
+    createFile, _ := os.ReadFile("../../tests/caller_identity/data/TestAccCallerIdentityDataSource.tf")
+
+    resource.Test(t, resource.TestCase{
+        ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+        Steps: []resource.TestStep{
+            {
+                Config: string(createFile),
+                Check: resource.ComposeAggregateTestCheckFunc(
+                    resource.TestCheckResourceAttrSet(
+                        testCallerIdentityDataSource,
+                        "tenant_id",
+                    ),
+                ),
+            },
+        },
+    })
 }
 
-func TestAccCallerIdentityDataSource(t *testing.T) {
-	createFile, err := os.ReadFile("../../tests/caller_identity/data/TestAccCallerIdentityDataSource.tf")
-	if err != nil {
-		t.Fatalf("failed to read config file: %s", err)
-	}
+func TestAccCallerIdentityDataSource_SecondaryProvider(t *testing.T) {
+    createFile, _ := os.ReadFile("../../tests/caller_identity/data/TestAccCallerIdentityDataSource_SecondaryProvider.tf")
 
-	config := string(createFile)
-
-	resource.Test(t, resource.TestCase{
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			{
-				Config: config,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet(testCallerIdentityDataSource, "tenant_id"),
-					testCallerIdentityTenantID(testCallerIdentityDataSource),
-				),
-			},
-		},
-	})
+    resource.Test(t, resource.TestCase{
+        ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+        Steps: []resource.TestStep{
+            {
+                Config: string(createFile),
+                Check: resource.ComposeAggregateTestCheckFunc(
+                    resource.TestCheckResourceAttrSet(
+                        testCallerIdentitySecondaryDataSource,
+                        "tenant_id",
+                    ),
+                ),
+            },
+        },
+    })
 }
