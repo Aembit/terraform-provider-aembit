ac41a7461989246e8be5d6d42cbb6d651161e166
Burak <burak@aembit.io>
2025-10-21 17:57:42 +0300

    Update TestAccCredentialProviderResource.tf

--- tests/credential/apikey/TestAccCredentialProviderResource.tf
+++ tests/credential/apikey/TestAccCredentialProviderResource.tf
@@ -1,10 +1,4 @@
 provider "aembit" {
-  default_tags {
-    tags = {
-      Name           = "Terraform Agent"
-      Owner          = "Aembit DevOps"
-    }
-  }
 }
 
 resource "aembit_credential_provider" "api_key" {
