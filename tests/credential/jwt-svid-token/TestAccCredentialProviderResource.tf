e919f6a19f768955ff285d7b4ec9465f985b51f9
Melda <mbilgili@aembit.io>
2025-11-17 17:33:58 +0300

    BAC-5331: TF Provider: Process Hash: Dynamic Claim Passthru (#184)
    * BAC-5331: TF Provider: Process Hash: Dynamic Claim Passthru
    
    * BAC-5331: fix for value
    
    * BAC-5331: fix for reference to undeclared resource error

--- tests/credential/jwt-svid-token/TestAccCredentialProviderResource.tf
+++ tests/credential/jwt-svid-token/TestAccCredentialProviderResource.tf
@@ -49,3 +49,35 @@ resource "aembit_credential_provider" "jwt_svid_token_null_custom_claims" {
 		algorithm_type = "ES256"		
 	}
 }
+
+resource "aembit_credential_provider" "jwt_svid_token_dynamic_subject_process_hash" {
+	name = "TF Acceptance JWT-SVID Token - DynamicSubjectProcessHash"
+	is_active = true
+	jwt_svid_token = {
+		subject = "spiffe://test.com/$${client.executable.hash.sha256}"
+		subject_type = "dynamic"
+		lifetime_in_minutes = 60
+		audience = "test.aembit.io"
+		algorithm_type = "ES256"
+		custom_claims = []	
+	}
+}
+
+resource "aembit_credential_provider" "jwt_svid_token_dynamic_claim_process_hash" {
+	name = "TF Acceptance JWT-SVID Token - DynamicClaimProcessHash"
+	is_active = true
+	jwt_svid_token = {
+		subject = "spiffe://test.com"
+		subject_type = "literal"
+		lifetime_in_minutes = 60
+		audience = "test.aembit.io"
+		algorithm_type = "ES256"
+		custom_claims = [
+			{
+				key = "key"
+				value = "spiffe://test.com/$${client.executable.hash.sha256}"
+				value_type = "dynamic"
+			}
+		]		
+	}
+}
