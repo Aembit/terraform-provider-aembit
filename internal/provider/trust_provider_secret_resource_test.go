19057779d1bf074d7e9f03c5c30a90320c664e9c
David Walker <david.walker@quantifex.com>
2025-12-02 18:06:36 -0500

    BAC-5573: Unit test fix, autogenerated PEM cert

--- internal/provider/trust_provider_secret_resource_test.go
+++ internal/provider/trust_provider_secret_resource_test.go
@@ -1,8 +1,17 @@
 package provider
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"log"
+	"math/big"
 	"os"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
@@ -18,13 +27,19 @@ func TestAccTrustProviderSecretResource(t *testing.T) {
 	modifyFile, _ := os.ReadFile(
 		"../../tests/trust_provider_secret/TestAccTrustProviderSecretResource.tfmod",
 	)
+	pemCertificate := generateRandomPEMCertificate(t, "Aembit Unit Test")
+	updatedPemCertificate := generateRandomPEMCertificate(t, "Updated Aembit Unit Test")
+
+	// Replace placeholder with generated certificate
+	createFileString := strings.Replace(string(createFile), "PEM_CERTIFICATE", pemCertificate, -1)
+	modifyFileString := strings.Replace(string(modifyFile), "PEM_CERTIFICATE", updatedPemCertificate, -1)
 
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Create and Read testing
 			{
-				Config: string(createFile),
+				Config: createFileString,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify dynamic values have any value set in the state.
 					resource.TestCheckResourceAttrSet(trustProviderSecretPath, "id"),
@@ -34,7 +49,7 @@ func TestAccTrustProviderSecretResource(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						trustProviderSecretPath,
 						"subject",
-						"CN=Test Certificate",
+						"CN=Aembit Unit Test",
 					),
 				),
 			},
@@ -42,17 +57,61 @@ func TestAccTrustProviderSecretResource(t *testing.T) {
 			{ResourceName: trustProviderSecretPath, ImportState: true, ImportStateVerify: false},
 			// Update and Read testing
 			{
-				Config: string(modifyFile),
+				Config: modifyFileString,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Verify Subject updated
 					resource.TestCheckResourceAttr(
 						trustProviderSecretPath,
 						"subject",
-						"CN=Different Test Certificate",
+						"CN=Updated Aembit Unit Test",
 					),
 				),
 			},
 			// Delete testing automatically occurs in TestCase
 		},
 	})
+}
+
+func generateRandomPEMCertificate(t *testing.T, commonName string) string {
+	var priv *rsa.PrivateKey
+	var err error
+	priv, err = rsa.GenerateKey(rand.Reader, 2048)
+
+	keyUsage := x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign
+	var notBefore time.Time = time.Now()
+	notAfter := notBefore.Add(time.Hour)
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		log.Fatalf("Failed to generate serial number: %v", err)
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			CommonName: commonName,
+		},
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
+
+		KeyUsage:              keyUsage,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+	}
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	if err != nil {
+		log.Fatalf("Failed to create certificate: %v", err)
+	}
+
+	block := &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: derBytes,
+	}
+
+	// Encode the pem.Block to a byte slice
+	pemBytes := pem.EncodeToMemory(block)
+
+	return string(pemBytes)
 }
--- tests/trust_provider_secret/TestAccTrustProviderSecretResource.tf
+++ tests/trust_provider_secret/TestAccTrustProviderSecretResource.tf
@@ -11,21 +11,6 @@ resource "aembit_trust_provider" "certificate_signed_attestation" {
 resource "aembit_trust_provider_secret" "secret" {
 	trust_provider_id = aembit_trust_provider.certificate_signed_attestation.id
 	secret      = <<-EOT
------BEGIN CERTIFICATE-----
-MIIC8jCCAdqgAwIBAgIJAMIlrM/czmegMA0GCSqGSIb3DQEBCwUAMBsxGTAXBgNVBAMTEFRlc3Qg
-Q2VydGlmaWNhdGUwHhcNMjUxMTE2MTU0MTEyWhcNMjUxMjAyMTU0MTEyWjAbMRkwFwYDVQQDExBU
-ZXN0IENlcnRpZmljYXRlMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsjBXXaG9PF3Q
-54xh3xr39IoW42RM6YTHWm2QgcD56uZW2YXkhLXvbiDWXA1RPa4Tfhy8UheimNosE0t3/fudB3jL
-J8S22k/60X9tuGosWU5rV68/Xnhsd+gFp1SwB2S1icRQKxiqxwtbH1ukYMSWcQ4kT/U0bbe/9G5V
-QWr8NHpPgTGMSIEzSyRLaEBtAvZsqkv+4bGfXDBo/AFqwGyJavhrln2I/550QyjtlthhqaH/lrnJ
-WirThM6M9s0RU7W2h6El37AT7/PUFd5nzi8JVFkerw8nzS6fLYcUgJb5CTVwIy/wKGf3mAKPlIN0
-JpX2OVt9nMe0sLt6972fjH/CvQIDAQABozkwNzAJBgNVHRMEAjAAMAsGA1UdDwQEAwIFoDAdBgNV
-HQ4EFgQUU+7wxT6cp2uaux6+/gBK3h1YOjswDQYJKoZIhvcNAQELBQADggEBAI2t0HxjH26BND6H
-QbJCCu7cj/tuGAaFIuubeccrtnHsbMkKVmOCxfNm2PPDcxcGXQf1pGbRnYWFwR/UkJm+ZlGIZfXR
-Z8F/+wor+hya0uSXppD1Qd3K/l1BiJAn5huMvlGppx56mzlqmB+h8ahZKeW0YvweJYpvuRFlL/p5
-rwfsG/EGzCYaJeS7o/k7dxtaDw5XsHvNVZy72nboG9hEUMwyfeCW/rYHbNWZDe3g08NfwrDosuZm
-ZdKgpyi48slfLVoiYRJPkZqF+TP8JBq3EIkH7xSdUtpkoLR6bgnTfYvWTW+4KS3vXJhHnlFYNBrk
-Ph3gNoIYBZi/TerC1Sao0k8=
------END CERTIFICATE-----
+PEM_CERTIFICATE
 EOT	
 }
