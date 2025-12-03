228e729f8e7172f5aefb5354768b11b012305c8d
David Walker <david.walker@quantifex.com>
2025-12-02 22:22:18 -0500

    BAC-5573: Unit Test cleanup

--- internal/provider/test.go
+++ internal/provider/test.go
@@ -2,9 +2,15 @@ package provider
 
 import (
 	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"fmt"
 	"math/big"
 	"strings"
+	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
@@ -62,4 +68,47 @@ func checkValidClientID(resourceName, attributeName, arnType string) resource.Te
 		}
 		return nil
 	}
+}
+
+func generateRandomPEMCertificate(t *testing.T, commonName string) string {
+	var priv *rsa.PrivateKey
+	var err error
+	priv, err = rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Errorf("Failed to generate private key: %v", err)
+	}
+
+	keyUsage := x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign
+	var notBefore = time.Now()
+	notAfter := notBefore.Add(time.Hour)
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		t.Errorf("Failed to generate serial number: %v", err)
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			CommonName: commonName,
+		},
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
+
+		KeyUsage:              keyUsage,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+	}
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template,
+		&priv.PublicKey, priv)
+	if err != nil {
+		t.Errorf("Failed to create certificate: %v", err)
+	}
+
+	// Encode the pem.Block to a byte slice
+	pemBytes := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+
+	return string(pemBytes)
 }
--- internal/provider/trust_provider_secret_resource_test.go
+++ internal/provider/trust_provider_secret_resource_test.go
@@ -1,16 +1,9 @@
 package provider
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
-	"math/big"
 	"os"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
@@ -72,47 +65,4 @@ func TestAccTrustProviderSecretResource(t *testing.T) {
 			// Delete testing automatically occurs in TestCase
 		},
 	})
-}
-
-func generateRandomPEMCertificate(t *testing.T, commonName string) string {
-	var priv *rsa.PrivateKey
-	var err error
-	priv, err = rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		t.Errorf("Failed to generate private key: %v", err)
-	}
-
-	keyUsage := x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign
-	var notBefore = time.Now()
-	notAfter := notBefore.Add(time.Hour)
-
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	if err != nil {
-		t.Errorf("Failed to generate serial number: %v", err)
-	}
-
-	template := x509.Certificate{
-		SerialNumber: serialNumber,
-		Subject: pkix.Name{
-			CommonName: commonName,
-		},
-		NotBefore: notBefore,
-		NotAfter:  notAfter,
-
-		KeyUsage:              keyUsage,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		BasicConstraintsValid: true,
-		IsCA:                  true,
-	}
-	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template,
-		&priv.PublicKey, priv)
-	if err != nil {
-		t.Errorf("Failed to create certificate: %v", err)
-	}
-
-	// Encode the pem.Block to a byte slice
-	pemBytes := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
-
-	return string(pemBytes)
 }
