5d4895d0a7a2dce06899ed93a7748dfe56049791
auzunel-broadangle <ali.uzunel@broadangle.com>
2026-03-11 22:08:00 +0300

    Linter updates.

--- internal/provider/client_workload_resource.go
+++ internal/provider/client_workload_resource.go
@@ -396,7 +396,6 @@ func convertClientWorkloadModelToDTO(
 	var identities []models.IdentitiesModel
 	if len(model.Identities.Elements()) > 0 {
 		_ = model.Identities.ElementsAs(ctx, &identities, false)
-		
 
 		for _, identity := range identities {
 			workload.Identities = append(workload.Identities, aembit.ClientWorkloadIdentityDTO{
@@ -453,11 +452,11 @@ func newClientWorkloadIdentityModel(
 		identities[i] = models.IdentitiesModel{
 			Type:  types.StringValue(identity.Type),
 			Value: types.StringValue(identity.Value),
-			Key:   func() types.String {
-			    if identity.Key == "" {
-			        return types.StringNull()
-			    }
-			    return types.StringValue(identity.Key)
+			Key: func() types.String {
+				if identity.Key == "" {
+					return types.StringNull()
+				}
+				return types.StringValue(identity.Key)
 			}(),
 		}
 	}
